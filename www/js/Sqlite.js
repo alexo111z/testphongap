b56f983cdc4367f38d7d57204e30758f70928033
alexo111z <alexo111y@gmail.com>
2020-01-17 12:49:17 -0600

    inicio de secion, devolucion de token de usuario

--- www/js/Sqlite.js
+++ www/js/Sqlite.js
@@ -1,14 +1,47 @@
+ 	url = 'http://192.168.1.72:8000/';
+   login = 'api/login';
+   
+   POST_login = url + login;
+
+function IniciarSesion(){
+	axios({
+		method: 'post',
+		url: POST_login,
+		data: {
+			email: $('#email').val(),
+			password: $('#password').val(),
+		}
+	})
+	.then(function(response){
+		if(response.status == 401){
+			alert('Usuario y/o contraseña invalidos.');
+		}
+		alert(response.data.access_token);
+		console.log(response);
+	})
+	.catch(function(error){
+		if(error.response.status == 401){
+			alert('Usuario y/o contraseña invalidos.');
+		}else {
+			console.log(error);
+		}
+		
+	})
+}
+
 
 jQuery(document).on("click",'#login',function(e){
-       window.sqlitePlugin.echoTest(function(){
+	console.log('IniciarSesion');
+	console.log(POST_login);
+	IniciarSesion();
+	/*window.sqlitePlugin.echoTest(function(){
 		alert("Prueba SQLite");
 		console.log('done sqlite');
-		document.getElementById("texto").innerHTML = "Hola Marte!!!!";
-		});
-    });
+	});*/
+});
+jQuery(document).on("load",'.login-form', function(e){
+	alert('Form cargado');
+})
 jQuery(document).ready(function(){
-	 window.sqlitePlugin.echoTest(function(){
-		alert("Prueba SQLite");
-		
-		});
+	alert('cargo.... done');
 });
