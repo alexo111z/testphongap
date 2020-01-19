dafebfb8262cc84d63c30ac691cd9bbb8dbbe8e2
alexo111z <alexo111y@gmail.com>
2020-01-18 23:18:41 -0600

    login

--- www/js/Sqlite.js
+++ www/js/Sqlite.js
@@ -34,61 +34,39 @@ function IniciarSesion(){
 	})
 }
 
-function InicioConJQuery(){
-	jQuery.ajax({
-		url: POST_login,
-		type: 'post',
-		data: {
-			email: $('#email').val(),
-			password: $('#password').val(),
-		}
-	}).done(function(data){
-		
-		console.log(data.access_token);
-		console.log('efectivamente logueado');
-		console.log(data);
-	}).fail(function(jqXHR, exception){
-		var msg = '';
-		if (jqXHR.status === 0) {
-				msg = 'Not connect.\n Verify Network.';
-		} else if (jqXHR.status == 404) {
-				msg = 'Requested page not found. [404]';
-		} else if (jqXHR.status == 500) {
-				msg = 'Internal Server Error [500].';
-		} else if (exception === 'parsererror') {
-				msg = 'Requested JSON parse failed.';
-		} else if (exception === 'timeout') {
-				msg = 'Time out error.';
-		} else if (exception === 'abort') {
-				msg = 'Ajax request aborted.';
-		} else {
-				msg = 'Uncaught Error.\n' + jqXHR.responseText;
-		}
-		alert("Error al actualizar, intentelo más tarde: "+msg);
-		console.log(msg);
-	});
-}
-
 
 jQuery(document).on("click",'#login',function(e){
 
-	console.log('IniciarSesion');
-	console.log(POST_login);
-	IniciarSesion();
+	 var data= {
+    	email: $('#email').val(),
+		password: $('#password').val(),
+  	};
 
-	/*window.sqlitePlugin.echoTest(function(){
-		alert("Prueba SQLite");
-		console.log('done sqlite');
-	});*/
+  axios.post(url + '/api/login',JSON.stringify(data))
+    .then(function(response){
+      console.log(response);
+      //alert(response.data.access_token);
+      var tok = response.data.access_token;
+      var id = response.data.id;
+      localStorage.setItem('token', tok)
+      localStorage.setItem('id', id)
+      //console.log(tok, id);
+      //window.location.href = 'perfil.html';
+    })
+    .catch(function(error){
+    	if (error.response.status == 401) {
+    		alert('Datos incorrecots');
+    	}
+      console.log(error.response);
 
-	//var db=window.sqlitePlugin.openDatabase({name: 'acceso.db', location: 'default'});
+    });
 
 });
 jQuery(document).on("load",'.login-form', function(e){
 	alert('Form cargado');
 })
 jQuery(document).ready(function(){
-	alert('cargo.... done');
+	//alert('cargo.... done');
 
 	/*// Store value on browser for duration of the session
 	sessionStorage.setItem('key', 'value');
@@ -97,9 +75,9 @@ jQuery(document).ready(function(){
 	alert(sessionStorage.getItem('key'));
 
 	// Store value on the browser beyond the duration of the session
-	localStorage.setItem('key', 'value');*/
+	localStorage.setItem('key', 'value');
 
 	// Retrieve value (persists even after closing and re-opening the browser)
-	alert(localStorage.getItem('key'));
+	alert(localStorage.getItem('key'));*/
 
 });
